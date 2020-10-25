446f4c91b2771e201641acd254e89ec4d18bd978
jangle <shubhamjangale0000@gmail.com>
2020-10-25 07:50:13 +0530

    Added : Added functionality to load the StateCensusData through test class

---
+++ src/test/java/census/analyser/CensusAnalyserTest.java
@@ -0,0 +1,23 @@
+package census.analyser;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+public class CensusAnalyserTest {
+
+	private static final String INDIA_CENSUS_CSV_FILE_PATH = "C://Users//DELL//eclipse-workspace//CensusAnalyser//IndiaStateCensusData.csv";
+
+	@Test
+	public void givenIndianCensusCSVFile_ReturnsCorrectRecords() {
+		try {
+			StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
+			int numOfRecords = stateCensusAnalyser.loadIndiaCensusData(INDIA_CENSUS_CSV_FILE_PATH);
+			assertEquals(29, numOfRecords);
+		} catch (IOException e) {
+		}
+	}
+
+}
